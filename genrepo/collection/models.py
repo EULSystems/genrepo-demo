aed653a63a74d4c8ef4a0c52b56f0017349612d4
rlskoeser <rebecca.s.koeser@emory.edu>
2011-05-05 22:18:30 +0000

    preliminary public-access xacml policies & cmodel place-holder #49
    git-svn-id: https://svn.library.emory.edu/svn/small_projects/generic-ingest/trunk@34 3b3aa91a-af69-469c-bc34-363fb7a4683f

--- genrepo/collection/models.py
+++ genrepo/collection/models.py
@@ -3,6 +3,15 @@ from django.db.models import Model
 
 from eulcore.fedora.models import DigitalObject
 
+class AccessibleObject(DigitalObject):
+    """A place-holder Fedora Object for auto-generating a PublicAccess
+    content model which will be used for Fedora XACML access controls.
+    """
+    PUBLIC_ACCESS_CMODEL = 'info:fedora/emory-control:PublicAccess'
+    CONTENT_MODELS = [ PUBLIC_ACCESS_CMODEL ]
+
+
+
 class Collection(Model):
     '''Collection place-holder object to define Django permissions on
     :class:`CollectionObject` . 
@@ -21,7 +30,7 @@ class CollectionObject(DigitalObject):
     model to identify this item as a Collection object.
     """
     COLLECTION_CONTENT_MODEL = 'info:fedora/emory-control:Collection-1.0'
-    CONTENT_MODELS = [ COLLECTION_CONTENT_MODEL ]
+    CONTENT_MODELS = [ COLLECTION_CONTENT_MODEL, AccessibleObject.PUBLIC_ACCESS_CMODEL ]
 
     # use configured fedora pidspace (if any) when minting pids
     default_pidspace = getattr(settings, 'FEDORA_PIDSPACE', None)
