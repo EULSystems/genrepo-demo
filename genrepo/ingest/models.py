e8d67a72f8430d506f9b4031e97ac429ca915c51
branker <branker@emory.edu>
2011-05-09 21:38:13 +0000

    add FileObject for uploaded files
    git-svn-id: https://svn.library.emory.edu/svn/small_projects/generic-ingest/trunk@52 3b3aa91a-af69-469c-bc34-363fb7a4683f

---
+++ genrepo/ingest/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from eulcore.fedora import DigitalObject
+from eulcore.fedora.models import FileDatastream
+from genrepo.collection.models import AccessibleObject
+
+class FileObject(DigitalObject):
+    '''An opaque file for repositing on behalf of a user. Inherits the
+    standard Dublin Core and RELS-EXT datastreams from
+    :class:`~eulcore.fedora.models.DigitalObject`, and adds both a
+    ``master`` datastream to contain the user's file as well as a content
+    model for identifying these objects.
+    '''
+    CONTENT_MODELS = [ AccessibleObject.PUBLIC_ACCESS_CMODEL ]
+
+    default_pidspace = getattr(settings, 'FEDORA_PIDSPACE', None)
+
+    master = FileDatastream("master", "reposited master file", defaults={
+            'versionable': True,
+        })
+    "reposited master :class:`~eulcore.fedora.models.FileDatastream`"
