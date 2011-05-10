43aff0c3eea7e6adac92fb247c424f8d73876eac
rlskoeser <rebecca.s.koeser@emory.edu>
2011-05-10 22:27:38 +0000

    preliminary xmlobjectform for editing Dublin Core metadata #34
    git-svn-id: https://svn.library.emory.edu/svn/small_projects/generic-ingest/trunk@63 3b3aa91a-af69-469c-bc34-363fb7a4683f

--- genrepo/file/forms.py
+++ genrepo/file/forms.py
@@ -1,6 +1,8 @@
-from django.forms import FileField, Form
+from django.forms import FileField, Form, TextInput, Textarea, ChoiceField
 
 from eulcore.django.forms.fields import DynamicChoiceField
+from eulcore.django.forms import XmlObjectForm
+from eulcore.xmlmap.dc import DublinCore
 
 from genrepo.collection.models import CollectionObject
 from genrepo.util import accessible
@@ -17,3 +19,38 @@ class IngestForm(Form):
     collection = DynamicChoiceField(choices=_collection_options, required=True,
         help_text="Add the new item to this collection.")
     file = FileField()
+
+class ReadOnlyInput(TextInput):
+    def __init__(self, *args, **kwargs):
+        readonly_attrs = {
+            'readonly':'readonly',
+            'class': 'readonly',
+            'tabindex': '-1'
+            }
+        if 'attrs' in kwargs:
+            kwargs['attrs'].update(readonly_attrs)
+        else:
+            kwargs['attrs'] = readonly_attrs
+        super(ReadOnlyInput, self).__init__(*args, **kwargs)
+
+
+class DublinCoreEditForm(XmlObjectForm):
+    """Form to edit Dublin Core metadata for a
+    :class:`~genrepo.file.models.FileObject`."""
+    # configure dc:type as a choice field populated by DCMI type vocabulary
+    _type_choices = [(t, t) for t in DublinCore().dcmi_types]
+    # add a blank value first so there is no default value
+    _type_choices.insert(0, (None, '')) 
+    type = ChoiceField(choices=_type_choices, required=False)
+
+    class Meta:
+        model = DublinCore
+        fields = ['title', 'description', 'creator', 'contributor',
+                  'date', 'coverage', 'language', 'publisher',
+                  'relation', 'rights', 'source', 'subject', 'type',
+                  'format', 'identifier']
+        widgets = {
+            'description': Textarea,
+            'format':  ReadOnlyInput,
+            'identifier': ReadOnlyInput,
+        }
