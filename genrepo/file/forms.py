0f11ba1ac2f159169f87702870f039ac0dd03076
rlskoeser <rebecca.s.koeser@emory.edu>
2011-05-10 21:38:16 +0000

    rollback accidental check-in of incomplete file DC edit form (undoing r60 for file/forms.py only)
    git-svn-id: https://svn.library.emory.edu/svn/small_projects/generic-ingest/trunk@61 3b3aa91a-af69-469c-bc34-363fb7a4683f

--- genrepo/file/forms.py
+++ genrepo/file/forms.py
@@ -1,8 +1,6 @@
-from django.forms import FileField, Form, Textarea, TextInput, ChoiceField
+from django.forms import FileField, Form
 
 from eulcore.django.forms.fields import DynamicChoiceField
-from eulcore.django.forms import XmlObjectForm
-from eulcore.xmlmap.dc import DublinCore
 
 from genrepo.collection.models import CollectionObject
 from genrepo.util import accessible
@@ -19,33 +17,3 @@ class IngestForm(Form):
     collection = DynamicChoiceField(choices=_collection_options, required=True,
         help_text="Add the new item to this collection.")
     file = FileField()
-
-class ReadOnlyInput(TextInput):
-    def __init__(self, *args, **kwargs):
-        readonly_attrs = {
-            'readonly':'readonly',
-            'class': 'readonly',
-            'tabindex': '-1'
-            }
-        if 'attrs' in kwargs:
-            kwargs['attrs'].update(readonly_attrs)
-        else:
-            kwargs['attrs'] = readonly_attrs
-        super(ReadOnlyInput, self).__init__(*args, **kwargs)
-
-
-class EditDublinCore(XmlObjectForm):
-    """Form to edit Dublin Core metadata."""
-    type = ChoiceField(choices=[(t, t) for t in DublinCore.DCMI_TYPES.keys()])
-
-    class Meta:
-        model = DublinCore
-        fields = ['title', 'description', 'creator', 'contributor',
-                  'date', 'coverage', 'language', 'publisher',
-                  'relation', 'rights', 'source', 'subject', 'type',
-                  'format', 'identifier']
-        widgets = {
-            'description': Textarea,
-            'format':  ReadOnlyInput,
-            'identifier':  TextInput(attrs={'readonly':'readonly', 'class': 'readonly', 'tabindex': '-1'}),
-        }
